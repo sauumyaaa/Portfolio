6d4abb83efb072abea189a50534a8f222d010193
saumyahere <sauumyaaa@gmail.com>
2024-07-26 01:18:09 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import pic from "../assets/pic.jpg";
 import { motion } from "framer-motion";
 
-const container = (duration,delay) => ({
+const container = (duration, delay) => ({
   hidden: { x: -100, opacity: 0 },
   visible: {
     x: 0,
@@ -21,7 +21,7 @@ const Hero = () => {
               // initial={{x:-100,opacity:0}}
               // animate={{x:0,opacity:1}}
               // transition={{duration:1,delay:1}}
-              variants={container(0.5,0.5)}
+              variants={container(0.5, 0.5)}
               initial="hidden"
               animate="visible"
               className="pb-16 text-6xl font-thin tracking-tight mt-4 lg:mt-10 lg:text-7xl"
@@ -30,37 +30,39 @@ const Hero = () => {
             </motion.h1>
             <motion.span
               // className='text-4xl bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 text-transparent bg-clip-text  tracking-tight '>
-              
-              animate={{rotate:360}}
-       transition={{ type:"spring" , duration: 4}}
+
+              animate={{ rotate: 360 }}
+              transition={{ type: "spring", duration: 4 }}
               className="text-4xl bg-gradient-to-r from-green-800 via-green-400 to-green-700 text-transparent bg-clip-text tracking-tight "
             >
               Tech Enthusiast
             </motion.span>
             <motion.p
-              variants={container(1,1.4)}
+              variants={container(1, 1.4)}
               initial="hidden"
               animate="visible"
               className="my-2 max-w-xl py-6 font-light tracking-tighter"
             >
-              I am a passionate full stack developer with a knack for crafting
-              robust and scalable web applications. With 5 years of hands-on
-              experience, I have honed my skills in front-end technologies like
-              React and Next.js, as well as back-end technologies like Node.js,
-              MySQL, PostgreSQL, and MongoDB. My goal is to leverage my
-              expertise to create innovative solutions that drive business
-              growth and deliver exceptional user experiences.
+              I am a third-year MCA student at Maulana Azad National Institute
+              of Technology, Bhopal, passionate about technology and innovation. With a
+              strong background in Mathematics and a keen interest in
+              technologies such as JavaScript, React, MySQL, and artificial intelligence, I thrive on applying mathematical and logical concepts
+              to practical challenges. Enthusiastic about problem-solving, I
+              enjoy learning through hands-on projects and am always eager to
+              explore new technologies and enhance my skills.
             </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <motion.img 
-             initial={{x:100,opacity:0}}
-             animate={{x:0,opacity:1}}
-             transition={{duration:3,delay:1.2}}
-        
-             className="rounded-2xl" src={pic} alt="pic" />
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 3, delay: 1.2 }}
+              className="rounded-2xl"
+              src={pic}
+              alt="pic"
+            />
           </div>
         </div>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,6 +14,7 @@ function Projects() {
               className='my-20 text-center text-4xl'>
             Projects</motion.h2>
             <div>
+
                 <div className='mb-12 flex flex-wrap lg:justify-center'>
                     <motion.div
                           whileInView={{ opacity: 1, x: 0 }}
@@ -21,10 +22,10 @@ function Projects() {
                           transition={{ duration: 1}}
                       className='w-full lg:w-1/4'>
                         <img
-                        src={pic2} 
-                        alt='password generator'
-                        width={160}
-                        height={160} 
+                        src="https://images.unsplash.com/photo-1516570161787-2fd917215a3d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fG1vbmV5fGVufDB8fDB8fHww" 
+                        alt='Currency Convertor'
+                        width={250}
+                        height={180} 
                         className='m-6 rounded-lg'
                         />
                     </motion.div>
@@ -37,46 +38,225 @@ function Projects() {
                       className='w-full max-w-xl lg:w-3/4'>
                         <h6 className='mb-2 font-semibold'>Currency Convertor</h6>
                         <p className='mb-4 text-neutral-400'>
-                        The SecurePassGen project aims to create a robust and user-friendly password generator application.
-                        This application will provide users with the capability to generate strong, random passwords that 
-                        adhere to various security policies and complexity requirements. The generated passwords will be 
-                        suitable for different use cases, from personal accounts to corporate systems.
+                        Created a responsive currency converter application to facilitate real-time currency exchange calculations
+                        using React and Tailwind CSS.
+Integrated a third-party API for real-time currency exchange.
+Learnt custom hooks from this project.
                         </p>
-                        <span className='mr-2 rounded bg-neutral-800 px-2 py-1 text-small font-medium text-purple-700'>HTML</span>
-                        <span className='mr-2 rounded bg-neutral-800 px-2 py-1 text-small font-medium text-purple-700'>CSS</span>
-                        <span className='mr-2 rounded bg-neutral-800 px-2 py-1 text-small font-medium text-purple-700'>JavaScript</span>
+                        <div >
+                          <a href="https://github.com/sauumyaaa/currency-converter" target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>CODE</a>
+                          <a href="https://sauumyaaa.github.io/currency-converter/" target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>DEMO</a>
+             </div>
                     </motion.div>
                 </div>
 
 
                 <div className='mb-12 flex flex-wrap lg:justify-center'>
-                    <div
+                    <motion.div
+                     whileInView={{ opacity: 1, x: 0 }}
+                     initial={{ opacity: 0, x: -100 }}
+                     transition={{ duration: 1}}
                     
                       className='w-full lg:w-1/4'>
                         <img
-                        src={pic1} 
+                        src="https://plus.unsplash.com/premium_photo-1681487746049-c39357159f69?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                         alt='password generator'
-                        width={160}
-                        height={160} 
+                        width={250}
+                        height={180} 
                         className='m-6 rounded-lg'
                         />
-                    </div>
+                    </motion.div>
 
-                    <div 
+                    <motion.div 
+                     whileInView={{ opacity: 1, x: 0 }}
+                     initial={{ opacity: 0, x: 100 }}
+                     transition={{ duration: 1 }}
+                  
                      
                       className='w-full max-w-xl lg:w-3/4'>
                         <h6 className='mb-2 font-semibold'>PassWord Generator</h6>
                         <p className='mb-4 text-neutral-400'>
-                        The GlobalCurrency project aims to create a dynamic and user-friendly currency converter application.
-                        This application will provide users with the ability to convert between various currencies using real-time
-                        exchange rates. The goal is to offer a reliable and convenient tool for travelers, businesses, and anyone 
-                        needing quick access to up-to-date currency conversions.
+                        Developed a tool for generating strong passwords using React and Tailwind CSS.
+Implemented various React hooks to manage state and handle user interactions.
+
                         </p>
-                        <span className='mr-2 rounded bg-neutral-800 px-2 py-1 text-small font-medium text-purple-700'>HTML</span>
-                        <span className='mr-2 rounded bg-neutral-800 px-2 py-1 text-small font-medium text-purple-700'>CSS</span>
-                        <span className='mr-2 rounded bg-neutral-800 px-2 py-1 text-small font-medium text-purple-700'>JavaScript</span>
-                    </div>
+                        <div >
+                          <a href="https://github.com/sauumyaaa/password-generator" target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>CODE</a>
+                          <a href="https://sauumyaaa.github.io/password-generator/" target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>DEMO</a>
+</div> </motion.div>
                 </div>
+
+
+
+                
+                <div className='mb-12 flex flex-wrap lg:justify-center'>
+                    <motion.div
+                     whileInView={{ opacity: 1, x: 0 }}
+                     initial={{ opacity: 0, x: -100 }}
+                     transition={{ duration: 1}}
+                    
+                      className='w-full lg:w-1/4'>
+                        <img
+                        src="https://images.unsplash.com/photo-1610647752706-3bb12232b3ab?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGFydGlmaWNpYWwlMjBuZXVyYWwlMjBuZXR3b2tycyUyMGNhcnxlbnwwfHwwfHx8MA%3D%3D"
+                        alt='Self Driving Car Simulation'
+                        width={250}
+                        height={180}
+                        className='m-6 rounded-lg'
+                        />
+                    </motion.div>
+
+                    <motion.div 
+                     whileInView={{ opacity: 1, x: 0 }}
+                     initial={{ opacity: 0, x: 100 }}
+                     transition={{ duration: 1 }}
+                  
+                     
+                      className='w-full max-w-xl lg:w-3/4'>
+                        <h6 className='mb-2 font-semibold'>Self Driving Car Simulation</h6>
+                        <p className='mb-4 text-neutral-400'>
+                        A Neural Networks and Machine Learning project using JavaScript with no libraries.
+                        Learnt how artificial neural networks work and implemented it along with visualization.
+                        </p>
+                        <div >
+                          <a href="https://github.com/sauumyaaa/selfDrivingCar" target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>CODE</a>
+                          <a href="https://sauumyaaa.github.io/selfDrivingCar/" target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>DEMO</a>
+          </div></motion.div>
+                </div>
+
+
+
+
+                <div className='mb-12 flex flex-wrap lg:justify-center'>
+                    <motion.div
+                     whileInView={{ opacity: 1, x: 0 }}
+                     initial={{ opacity: 0, x: -100 }}
+                     transition={{ duration: 1}}
+                    
+                      className='w-full lg:w-1/4'>
+                        <img
+                        src="https://plus.unsplash.com/premium_photo-1718884970260-4c8cb02d7941?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YnJlYXN0JTIwY2FuY2VyfGVufDB8fDB8fHww"
+                        alt='Breast Cancer detection using CNN'
+                        width={250}
+                        height={180} 
+                        className='m-6 rounded-lg'
+                        />
+                    </motion.div>
+
+                    <motion.div 
+                     whileInView={{ opacity: 1, x: 0 }}
+                     initial={{ opacity: 0, x: 100 }}
+                     transition={{ duration: 1 }}
+                  
+                     
+                      className='w-full max-w-xl lg:w-3/4'>
+                        <h6 className='mb-2 font-semibold'>Breast Cancer detection using CNN</h6>
+                        <p className='mb-4 text-neutral-400'>
+                        Used Convolutional Neural Networks for detecting breast cancer
+                        Datasets used: UCI ML Breast Cancer Wisconsin (Diagnostic) datasets.
+Models achieved exceptional training and validation accuracies of 99.78% and 97.37% respectively.
+                        </p>
+                        <div >
+                          <a href="https://github.com/sauumyaaa/breastCancerCNN/blob/master/CNN_for_breast_cancer_detection.ipynb" target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>GITHUB</a>
+                          <a href="https://colab.research.google.com/github/sauumyaaa/breastCancerCNN/blob/master/CNN_for_breast_cancer_detection.ipynb" target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>GOOGLE COLAB</a>
+            </div> </motion.div>
+                </div>
+
+
+
+
+                <div className='mb-12 flex flex-wrap lg:justify-center'>
+                    <motion.div
+                     whileInView={{ opacity: 1, x: 0 }}
+                     initial={{ opacity: 0, x: -100 }}
+                     transition={{ duration: 1}}
+                    
+                      className='w-full lg:w-1/4'>
+                        <img
+                        src="https://images.pexels.com/photos/267569/pexels-photo-267569.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        alt='MANIT blogs'
+                        width={250}
+                        height={180}
+                        className='m-6 rounded-lg'
+                        />
+                    </motion.div>
+
+                    <motion.div 
+                     whileInView={{ opacity: 1, x: 0 }}
+                     initial={{ opacity: 0, x: 100 }}
+                     transition={{ duration: 1 }}
+                  
+                     
+                      className='w-full max-w-xl lg:w-3/4'>
+                        <h6 className='mb-2 font-semibold'>MANIT blogs</h6>
+                        <p className='mb-4 text-neutral-400'>
+                        Created a dynamic full-stack web application using React, MySQL, Node.js, and Express.js, enabling users to read and write blogs across various categories.
+
+
+
+Implemented robust authentication using JWT and cookies to ensure secure user sessions.
+                        </p>
+                        <div >
+                          <a  target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>soon</a>
+                          <a  target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>soon</a>
+              </div>
+               </motion.div>
+                </div>
+
+
+
+
+
+                <div className='mb-12 flex flex-wrap lg:justify-center'>
+                    <motion.div
+                     whileInView={{ opacity: 1, x: 0 }}
+                     initial={{ opacity: 0, x: -100 }}
+                     transition={{ duration: 1}}
+                    
+                      className='w-full lg:w-1/4'>
+                        <img
+                                               src="https://images.pexels.com/photos/3986952/pexels-photo-3986952.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+
+                        alt='Freelance Zone'
+                        width={250}
+                        height={180}
+                        className='m-6 rounded-lg'
+                        />
+                    </motion.div>
+
+                    <motion.div 
+                     whileInView={{ opacity: 1, x: 0 }}
+                     initial={{ opacity: 0, x: 100 }}
+                     transition={{ duration: 1 }}
+                  
+                     
+                      className='w-full max-w-xl lg:w-3/4'>
+                        <h6 className='mb-2 font-semibold'>Freelance Zone</h6>
+                        <p className='mb-4 text-neutral-400'>
+                        A dynamic job marketplace platform for connecting freelancers with clients.
+                        Developed a MERN stack web application where freelancers can post detailed gigs, including price and service descriptions.
+
+
+
+Clients can explore and select gigs based on their needs, budget, and freelancer ratings.
+
+
+
+Implemented secure authentication, user profiles, and a messaging system for seamless communication between buyers and sellers.
+                        </p>
+                        <div >
+                          <a target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>soon</a>
+                          <a  target='new' className= ' w-30 bg-violet-500 hover:bg-violet-600 p-2.5 rounded mx-2 font-bold'>soon</a>
+            </div>
+               </motion.div>
+                </div>
+
+
+
+
+
+
+
+
             </div>
         </div>
     )
